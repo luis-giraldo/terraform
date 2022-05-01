6ef00914be0a0e1326f679babc4873d24e82e7d0
luis <giraldo.luisdavid@gmail.com>
2022-05-01 12:51:04 -0500

    add user data

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "aws_instance" "my-instance" {
   associate_public_ip_address = true
   #key_name = "aws"
   key_name  = aws_key_pair.ssh-key.key_name
-  user_data                   = file("entry-script.sh")
+  user_data                   = file("entry.sh")
 
   tags = {
     Name = "${var.env}-instance"
